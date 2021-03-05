264cfabd443113927fd4905eb5d54d065d9c9acf
Andreas <andreas@jagelund.se>
2021-03-05 13:26:35 +0100

    initial commit

---
+++ Api/Controllers/WebhookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebhookController : ControllerBase
+    {
+        private readonly ILogger<WebhookController> _logger;
+
+        public WebhookController(ILogger<WebhookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Post([FromBody] HookRequest hookRequest)
+        {
+            _logger.LogInformation(hookRequest.text);
+
+            return Accepted();
+        }
+    }
+
+    public record HookRequest(string text);
+}
---
+++ DaClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DaClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
